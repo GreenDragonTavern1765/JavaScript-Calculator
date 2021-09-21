358cf164c4758f98c9902efb2cd6cd9dfe652701
Zachary <zacharypreston1996@protonmail.com>
2021-09-20 23:33:52 -0400

    Dark Mode added, similar to light.css, however the rgb values are slightly different. Additionally, the sources for the code are included. More will be added in the future, such as parentheses, scientific calculations, etc.

--- calculatorScript.js
+++ calculatorScript.js
@@ -1,3 +1,6 @@
+let lightTheme = "light.css";
+let darkTheme = "dark.css";
+
 // Clears the screen when the user clicks the "c" button
 function clearScreen() {
     document.getElementById("result").value = "";
@@ -9,4 +12,17 @@ function liveScreen(value) {
     if (res.value == "undefined") {
         res.value = "";
     } res.value += value;
+}
+
+// Swaps the style sheet to either dark mode or light mode
+function changeTheme() {
+    let darkMode = document.getElementById("dark-mode");
+    let theme = document.getElementById("theme");
+    if (theme.getAttribute("href") == lightTheme) {
+        theme.href = darkTheme;
+        darkMode.innerHTML = "Light Mode";
+    } else {
+        theme.href = lightTheme;
+        darkMode.innerHTML = "Dark Mode";
+    }
 }
